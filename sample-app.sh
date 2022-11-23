217b9c8fa550e1cd544c4a700b6e0d24ca47bb51
yahiahwewi <74486724+yahiahwewi@users.noreply.github.com>
2022-11-23 10:45:02 +0000

    Changed port from 8080 to 5050.

--- sample-app.sh
+++ sample-app.sh
@@ -1,22 +1,25 @@
 #!/bin/bash
-
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
-
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
-
-echo "FROM python" >> tempdir/Dockerfile
-echo "RUN pip install flask" >> tempdir/Dockerfile
-echo "COPY  ./static /home/myapp/static/" >> tempdir/Dockerfile
-echo "COPY  ./templates /home/myapp/templates/" >> tempdir/Dockerfile
-echo "COPY  sample_app.py /home/myapp/" >> tempdir/Dockerfile
-echo "EXPOSE 8080" >> tempdir/Dockerfile
-echo "CMD python /home/myapp/sample_app.py" >> tempdir/Dockerfile
-
+echo
+echo
+echo
+echo
+echo
+echo
+echo
+'FROM python' >> tempdir/Dockerfile
+"RUN pip install flask" >> tempdir/Dockerfile
+"COPY ./static /home/myapp/static/" >> tempdir/Dockerfile
+"COPY ./templates /home/myapp/templates/" >> tempdir/Dockerfile
+"COPY sample_app.py /home/myapp/" >> tempdir/Dockerfile
+"EXPOSE 5050" >> tempdir/Dockerfile
+"CMD python3 /home/myapp/sample_app.py" >> tempdir/Dockerfile
 cd tempdir
-docker build -t sampleapp .
-docker run -t -d -p 8080:8080 --name samplerunning sampleapp
-docker ps -a 
+docker build -t sampleapp.
+docker run -t -d -p 5050:5050 --name samplerunning sampleapp
+docker ps -a
